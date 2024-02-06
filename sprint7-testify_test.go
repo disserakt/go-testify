4122cb16b973badb89f888afc0e1275a974cef19
Roman Ananev <ryuananev@avito.ru>
2024-02-06 22:47:29 +0300

    go-testify-markI - commit markI

--- sprint7-testify_test.go
+++ sprint7-testify_test.go
@@ -6,6 +6,8 @@ import (
     "strconv"
     "strings"
     "testing"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -48,11 +50,31 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
     totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
+    // здесь нужно создать запрос к сервису
+    req := httptest.NewRequest("GET", "/cafe?count=6&city=moscow", nil)
 
     responseRecorder := httptest.NewRecorder()
     handler := http.HandlerFunc(mainHandle)
     handler.ServeHTTP(responseRecorder, req)
 
     // здесь нужно добавить необходимые проверки
+    
+    if status := responseRecorder.Code; status != http.StatusOK {
+        t.Errorf("expected status code: %d, got %d", http.StatusOK, status)
+    }
+    
+    if status := responseRecorder.Code; status != http.StatusBadRequest {
+        t.Errorf("expected status code: %d, got %d", http.StatusBadRequest, status)
+    }
+
+    expected := `wrong city value`
+    if responseRecorder.Body.String() != expected {
+        t.Errorf("expected body: %s, got %s", expected, responseRecorder.Body.String())
+    }
+    
+    countStr := req.URL.Query().Get("count")
+    count, _ := strconv.Atoi(countStr)
+    if count >= totalCount {
+        t.Errorf("expected body: %s, got %s", expected, responseRecorder.Body.String())
+    }
 }
